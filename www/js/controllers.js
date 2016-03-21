b8dfd8d949ba8201da3cc614c669ec807165bae2
Ali Ok <aliok@apache.org>
2016-03-21 01:27:52 +0100

    Cordova: Initial impl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,46 @@
-angular.module('app.controllers', [])
-  
-.controller('henryVisionCtrl', function($scope) {
+angular.module('app.controllers', ['app.services'])
 
-})
-   
-.controller('settingsCtrl', function($scope) {
+  .controller('henryVisionCtrl', ['$scope', '$q', 'Camera', 'HenryVision', function ($scope, $q, Camera, HenryVision) {
+    $scope.images = [];
 
-})
-    
+    $scope.takePhoto = function () {
+
+      var image = {
+        date: new Date(),
+        src: undefined,
+        text: undefined,
+        inProgress: "true",
+        error: undefined
+      };
+
+      Camera.getPicture()
+        .then(function (imageData) {
+          image.src = "data:image/png;base64," + imageData;
+          // add to beginning
+          $scope.images.unshift(image);
+
+          return imageData;
+        })
+        .then(HenryVision.getTextForImage)
+        .then(function (response) {
+          console.log(response);
+          image.text = response ? response.text : undefined;
+          image.status = undefined;
+          image.inProgress = false;
+        })
+        .catch(function (err) {
+          console.error(err);
+          if (err.message) {
+            image.error = err.message;
+          }
+          else {
+            image.error = "An error occurred.";
+          }
+        });
+    };
+
+  }])
+
+  .controller('settingsCtrl', ['$scope'], function ($scope) {
+
+  });
