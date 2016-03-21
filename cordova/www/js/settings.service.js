7dc85eadf8a7ef75cbf59ee6dde44b3bd873e154
Ali Ok <aliok@apache.org>
2016-03-21 14:30:51 +0100

    Cordova: filled about page and made TTS enabled by default

--- cordova/www/js/settings.service.js
+++ cordova/www/js/settings.service.js
@@ -9,15 +9,25 @@
   SettingsService.$inject = ['localStorageService'];
 
   function SettingsService(localStorageService) {
-    return {
+    var service = {
       setTTSEnabled: function (ttsEnabled) {
         localStorageService.set(TTS_ENABLED_KEY, ttsEnabled);
       },
 
       isTTSEnabled: function () {
-        return localStorageService.get(TTS_ENABLED_KEY);
+        var isEnabled = localStorageService.get(TTS_ENABLED_KEY);
+
+        // this library returns null, not undefined!
+        if (isEnabled === null) {
+          service.setTTSEnabled(true);
+          return true;
+        }
+        else {
+          return isEnabled;
+        }
       }
     };
 
+    return service;
   }
 })();
